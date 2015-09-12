962ecf7ad4875992810bae9ada7fca77d3f21f6f
feythin lau <feythin.lau@gmail.com>
2015-09-12 17:23:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+==========
+环境搭建
+
+安装 virtualenv环境
+
+    wget https://bootstrap.pypa.io/ez_setup.py -O - | python
+
+    easy_install virtualenv
+
+初始化环境
+
+    python tools/install_venv.py
+测试环境
+
+    easy_install tox
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright @ 2015 OPS, liufeiyu.cn
+# Author: feytin.lau <feythin.lau@gmail.com>
+#
+
+from setuptools import setup
+
+setup(
+        setup_requires=['pbr'],
+        pbr=True,
+
+)
