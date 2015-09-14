7011315f67f8b19fa45d21116bcafa476e8a3ddc
feythin lau <feythin.lau@gmail.com>
2015-09-14 10:19:10 +0800

    init

--- setup.py
+++ setup.py
@@ -4,11 +4,15 @@
 # Copyright @ 2015 OPS, liufeiyu.cn
 # Author: feytin.lau <feythin.lau@gmail.com>
 #
+import setuptools
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
-from setuptools import setup
-
-setup(
-        setup_requires=['pbr'],
-        pbr=True,
-
-)
+setuptools.setup(
+    setup_requires=['pbr>=1.3'],
+    pbr=True)
